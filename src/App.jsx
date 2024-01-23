e6e6fa077dc05d514eb8e12a1e0999d0c7ee9510
Jerin K Johnson <21bca130@sxca.edu.in>
2024-01-23 18:15:09 +0530

    created pages & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
-export default function App() {
-  return <h1 className='text-red-500'>App</h1>
+
+
+export default function App(){
+    return <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sign-in" element={<SignIn />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+        
+    </Routes>
+    </BrowserRouter>;
 }
