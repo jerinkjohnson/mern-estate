e9fc7793ff1217270574fec0e23c98845552fdb3
Jerin K Johnson <21bca130@sxca.edu.in>
2024-01-31 12:25:21 +0530

    header section UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from './components/Header';
 
 
 
 export default function App(){
     return <BrowserRouter>
+    <Header />
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Sign-in" element={<SignIn />} />
         <Route path="/Sign-up" element={<SignUp />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/About" element={<About />} />
-        
     </Routes>
     </BrowserRouter>;
 }
